80f186e09809301fd6a97dbe3ba7f4c855c82cf1
Sabina B <120465527+SabinaBeisembayevakz@users.noreply.github.com>
2024-12-02 13:52:40 -0800

    Add files via upload

---
+++ europharma.py
@@ -0,0 +1,125 @@
+import os
+import time
+import datetime
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from cloud_storage import *
+from load_to_bq import *
+import unicodedata2
+
+os.environ["DISPLAY"]=":99"
+
+def scraper (browser):
+    list_urls = []
+    data = []
+
+    url = "https://europharma.kz"
+    browser.get(url)
+    time.sleep(5)
+    print("Зашёл на Сайт")
+
+    link = browser.find_element(By.CLASS_NAME,'city-confirm__btn-confirm') 
+    link.click()
+    print("Выбран Город")
+
+    try1 = browser.find_element(By.XPATH,'//a[@class="nav__link js-dropdown-trigger"]')
+    browser.execute_script("arguments[0].click();", try1)
+    #press the button Katalog
+
+    urls=browser.find_elements(By.XPATH,"//*[@class='menu__link']")
+    for u in urls:
+        li=u.get_attribute('href')
+        list_urls.append(li)
+    print(list_urls)
+
+    for i in range(0, len(list_urls),1):
+    # for i in range(0,3,1):
+        Sub_urls = []
+        New_urls = []
+
+        browser.get(list_urls[i])
+        time.sleep(3)
+        b=1
+        # while True:
+        for i in range (1,2):
+            content = browser.page_source
+            soup = BeautifulSoup(content, features="html.parser")
+            
+            for i in soup.findAll("div", class_="catalog-list__item"):
+                link = i.find("a", href = True)
+                Sub_urls.append(link['href'])
+                
+            time.sleep(4)
+
+            try: 
+                Page2 = browser.find_element(by=By.XPATH, value='//a[@data-page="'+str(b)+'"]')
+                browser.execute_script("arguments[0].click();", Page2)
+                b = b + 1
+            except:
+                break
+        for i in Sub_urls:
+            helper = str(url) + str(i)
+            New_urls.append(helper)
+        print(len(New_urls))
+
+        for i in New_urls[0:2]:
+            browser.get(i)
+            content = browser.page_source
+            soup = BeautifulSoup(content, features="html.parser")
+            a1 = browser.find_elements(By.XPATH,"//span[@itemprop='name']")
+            
+            for i in soup.findAll("div", class_="product-detail"):
+                Name = i.find("h1", class_="product__title").text
+                Country = i.findAll("dd", class_="characteristic__value")[2].text
+                Cost = i.find("span", class_="product__price-value").text
+                try:
+                    category = a1[2].text
+                except:
+                    category = ""
+                try:
+                    subCategory = a1[3].text
+                except:
+                    subCategory = ""
+                day_today = datetime.today()
+                items = {
+                    'name': Name,
+                    'country': Country,
+                    'price': Cost,
+                    'category': category,
+                    'subcategory': subCategory,
+                    'date':day_today
+                }
+
+                data.append(items)
+
+    df = pd.DataFrame(data)
+
+    browser.close()
+    browser.quit()
+
+    return df
+
+def main():
+    project_name = 'pg-kz-n-app-847976'
+    dataset_name = 'europharma'
+    cloud_storage = 'europharma'
+
+    chrome_options = webdriver.ChromeOptions()
+    path = './chromedriver'
+    browser = webdriver.Chrome(executable_path=path, options = chrome_options)
+
+    df = scraper(browser)
+
+    df_raw = df.copy()
+    df_to_cloudstorage(cloud_storage, dataset_name, df_raw)
+    x = df['price']
+    x = x.replace(to_replace='₸', value='', regex=True).values
+    x = [unicodedata2.normalize('NFKD', str(x[i])).replace(' ','') for i in range (0, len(x))]
+    df['price'] = [float(i) for i in x]
+    print(df.head())
+    load_to_bq(project_name, dataset_name, df)
+
+main()
+
---
+++ rauza-ade (1).py
@@ -0,0 +1,123 @@
+import random
+import time
+import pandas as pd
+import os
+
+from datetime import datetime
+from bs4 import BeautifulSoup as bs
+from cloud_storage import *
+from load_to_bq import *
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+os.environ["DISPLAY"]=":99"
+
+def drop_dublicates(x):
+    return list(dict.fromkeys(x))
+
+
+def scraper(browser):
+    items=[]
+    errs = []
+    links1=[]
+    urls=[]
+
+    url = 'https://rauza-ade.kz/catalog'
+    browser.get(url)
+    time.sleep(2)
+
+    page_num = len(browser.find_elements(By.CSS_SELECTOR, 'div.page'))
+    if page_num == 0:
+        print("Oops! The website seems to be blocked!")
+    else:
+        print("Got the pages number!")
+        for num in range (1,page_num+1):
+        # for num in range (1,2):
+            page = url + '?p=' + str(num) + '&sort=popularity'
+            urls.append(page)
+            print("Page "+ str(num) + " -> " + str(page))
+
+        for u in urls:
+            browser.get(u)
+            time.sleep(2)
+            elem1 = browser.find_elements(By.CSS_SELECTOR, 'a.row')
+        
+            for i in elem1:
+                li = i.get_attribute('href')
+                # print(i.text)
+                links1.append(li)
+
+        links1 = drop_dublicates(links1)
+        print(links1)
+        print('Collected links ' + str(len(links1)))
+        for l in range(0, len(links1)):
+            try:
+                thelink = links1[l]
+                cats = []
+                print(thelink)
+                browser.get(thelink)
+                time.sleep(2)
+                c = browser.find_elements(by=By.CSS_SELECTOR, value='a')
+                for i in c:
+                    try:
+                        if '/catalog/' in i.get_attribute('href'):
+                            cats.append(i.text)
+                    except:
+                        cats.append('')
+                cat1 = cats[1]
+                cat2 = cats[2]
+                cat3 = cats[3]
+                name = browser.find_elements(by=By.CSS_SELECTOR, value='h1.title')[0].text
+                madein = browser.find_elements(by=By.CSS_SELECTOR, value='span.key')[0].text
+                company = browser.find_elements(by=By.CSS_SELECTOR, value='span.key')[1].text
+                price = browser.find_elements(by=By.CSS_SELECTOR, value='span.price')[0].text
+                dict_set = {
+                    'category':cat1,
+                    'subcategory': cat2,
+                    'group': cat3,
+                    'name': name,
+                    'country': madein,
+                    'company':company,
+                    'price':price,
+                    'url':thelink
+                }
+                items.append(dict_set)
+                print(str(l) + "/" + str(len(links1)) + " scraped links")
+            except:
+                print("Link " + str(l) + " has errors")
+                errs.append(thelink)
+
+    df = pd.DataFrame(items)
+    df['date'] = datetime.today()
+    if len(errs) > 0:
+        dferr = pd.DataFrame(errs)
+        dferr.to_excel('rauza-errs-links.xlsx')
+
+    print(str(len(errs))+' ERROR LINKS')       
+    print(df.head())
+   
+    browser.close()
+    browser.quit() 
+    return(df)
+
+
+def main():
+    
+    project_name = 'pg-kz-n-app-847976'
+    dataset_name = 'rauza_ade'
+    cloud_storage = 'rauza-ade'
+
+    chrome_options = Options()
+    path = './chromedriver'
+    browser = webdriver.Chrome(path, chrome_options=chrome_options)
+    df = scraper(browser)
+    df_raw = df.copy()
+    df_to_cloudstorage(cloud_storage, dataset_name, df_raw)
+    x = df['price'].replace(to_replace=' ', value='', regex=True)
+    y = x.replace(to_replace='₸', value='', regex=True)
+    df['price'] = [float(x) for x in y]
+    print(df.head())
+    load_to_bq(project_name, dataset_name, df)   
+
+main()
